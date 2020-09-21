4d93d7666e7b2a67dfaa7514208fbcb11ac45ad6
burvxs <benny.purvis@hotmail.com>
2020-09-21 17:37:18 +1000

    Added parent comp for task sets

---
+++ src/components/D2DTasks.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+
+const D2DTasks = () => {
+    const [] = useState([])
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default D2DTasks;
---
+++ src/components/Tasker.js
@@ -0,0 +1,15 @@
+/*
+    This component will be a parent component for other
+    task sets
+*/
+import React from 'react';
+
+const Tasker = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Tasker;
